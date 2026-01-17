37d19191b7449513053ca798918f6d3819442e54
Jesmo <26099018+JesmoDev@users.noreply.github.com>
2026-01-17 20:14:16 +0100

    Updates

--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/sw.js', { scope: '/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/glance-calendar/sw.js', { scope: '/glance-calendar/' })})}
